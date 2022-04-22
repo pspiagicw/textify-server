e77a44b001dbedbb5b9c473d5bc388f23ef9c73c
pspiagicw <pspiagicw@gmail.com>
2022-04-23 01:54:41 +0530

    Added more changes

--- textify/database.py
+++ textify/database.py
@@ -9,16 +9,16 @@ def setupDatabase():
     client = pymongo.MongoClient("mongodb+srv://pspiagicw:helloworldtotextify@cluster0.tfus8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     db = client['textify-data']
     
-def Set(id , data):
+def addUserData(id , data):
     collection = db['users-data']
-    final_data = dict(user =id , **data)
+    final_data = dict(uid =id , **data)
     data_uid = collection.insert_one(final_data).inserted_id
     return 500
 
-def GetAll(id):
+def getUserData(id):
     collection = db['users-data']
     documents = list()
-    cursor = collection.find({})
+    cursor = collection.find({'uid' : id })
     for i in cursor:
         documents.append(cursor)
     return 500 , documents
--- textify/main.py
+++ textify/main.py
@@ -6,7 +6,7 @@ from textify import database
 app = Flask(__name__)
 database.setupDatabase()
 
-@app.route('/new')
+@app.route('/login')
 def newlogin():
     data = {
         'id': uuid1(),
@@ -14,16 +14,16 @@ def newlogin():
     }
     return jsonify(data)
 
-@app.route('/get/<id>' , methods=['GET'])
+@app.route('/pull/<id>' , methods=['GET'])
 def getdata(id):
-    status , data = database.GetAll(id)
+    status , data = database.getUserData(id)
     return jsonify(data) , status
 
-@app.route('/set/<id>' , methods=['POST'])
+@app.route('/push/<id>' , methods=['POST'])
 def setdata(id):
     data = request.get_json()
-    print(data)
-    response = database.Set(id , data)
+    final_data = dict(uid = id , **data)
+    response = database.addUserData(id , final_data)
     return Response(status = response)
 
     
