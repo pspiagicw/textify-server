5d9b669e26e3944123ac3f37696eec6f07b985d8
pspiagicw <pspiagicw@gmail.com>
2022-04-23 00:27:45 +0530

    Initial Commit

---
+++ textify/database.py
@@ -0,0 +1,24 @@
+import pymongo
+
+
+
+
+db = None
+def setupDatabase():
+    global db
+    client = pymongo.MongoClient("mongodb+srv://pspiagicw:helloworldtotextify@cluster0.tfus8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    db = client['textify-data']
+    
+def Set(id , data):
+    collection = db['users-data']
+    data_uid = collection.insert_one(data).inserted_id
+    return 500
+
+def GetAll(id):
+    collection = db['users-data']
+    documents = list()
+    cursor = collection.find({})
+    for i in cursor:
+        documents.append(cursor)
+    return 500 , documents
+
---
+++ textify/main.py
@@ -0,0 +1,31 @@
+from flask import Flask , jsonify , request , Response
+from uuid import uuid1
+import time
+from server import database
+
+app = Flask(__name__)
+database.setupDatabase()
+
+@app.route('/new')
+def newlogin():
+    data = {
+        'id': uuid1(),
+        'timestamp': time.time()
+    }
+    return jsonify(data)
+
+@app.route('/get/<id>' , methods=['GET'])
+def getdata(id):
+    status , data = database.GetAll(id)
+    return jsonify(data) , status
+
+@app.route('/set/<id>' , methods=['POST'])
+def setdata(id):
+    data = request.get_json()
+    print(data)
+    response = database.Set(id , data)
+    return Response(status = response)
+
+    
+if __name__ == '__main__':
+    app.run(debug=True)
